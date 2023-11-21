08059881f6c52f5f6914728c592c534a8d9aa158
ainanyfitiagershom <118452611+ainanyfitiagershom@users.noreply.github.com>
2023-11-21 16:45:54 +0300

    React_front

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+
+ function tab(){
+	
+
+  return (
+    
+    <table class="container">
+		  <thead>
+			  <tr>
+         
+          <th>Saision</th>
+          <th>Match</th>
+		  <th>Equipe 1</th>
+          <th>Score 1</th>
+		  <th>Equipe 2</th>
+          <th>Score 2</th>
+		    </tr>
+		  </thead>
+		  <tbody>
+			<tr>
+        		<th>1</th>
+				<td>1</td>
+				<td>E1</td>
+				<td>80</td>
+				<td>E2</td>
+				<td>90</td>
+      </tr>
+	    </tbody>
+	  </table>
+  );
+}
+
+
+
+export default tab;
